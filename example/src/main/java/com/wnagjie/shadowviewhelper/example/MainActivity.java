e67c6cd240f37e436dd8bb8dc3fb09fd962c396c
王杰 <tiantian.china.2@gmail.com>
2015-05-25 17:20:08 +0800

    初始化第一个版本

---
+++ example/src/main/java/com/wnagjie/shadowviewhelper/example/MainActivity.java
@@ -0,0 +1,78 @@
+package com.wnagjie.shadowviewhelper.example;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import com.wangjie.androidbucket.utils.ABTextUtil;
+import com.wangjie.androidinject.annotation.annotations.base.AILayout;
+import com.wangjie.androidinject.annotation.annotations.base.AIView;
+import com.wangjie.androidinject.annotation.present.AIActionBarActivity;
+import com.wnagjie.shadowviewhelper.ShadowProperty;
+import com.wnagjie.shadowviewhelper.ShadowViewHelper;
+
+@AILayout(R.layout.activity_main)
+public class MainActivity extends AIActionBarActivity {
+    @AIView(R.id.activity_main_shadow_view_a)
+    private View shadowViewA;
+
+    @AIView(R.id.activity_main_shadow_view_b)
+    private View shadowViewB;
+
+    @AIView(R.id.activity_main_shadow_view_c)
+    private View shadowViewC;
+    @AIView(R.id.activity_main_shadow_view_c_iv)
+    private View shadowImageC;
+
+    @AIView(R.id.activity_main_shadow_view_d)
+    private View shadowImageD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // View A
+        ShadowProperty shadowProperty = new ShadowProperty()
+                .setShadowColor(0x77000000)
+                .setShadowRadius(ABTextUtil.dip2px(context, 4));
+
+        ShadowViewHelper.bindShadowHelper(shadowProperty, shadowViewA);
+        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) shadowViewA.getLayoutParams();
+        lp.leftMargin = -shadowProperty.getShadowOffset();
+        lp.rightMargin = -shadowProperty.getShadowOffset();
+        shadowViewA.setLayoutParams(lp);
+
+
+        // View B
+        ShadowViewHelper.bindShadowHelper(
+                new ShadowProperty()
+                        .setShadowColor(0x77000000)
+                        .setShadowDy(ABTextUtil.dip2px(context, 0.5f))
+                        .setShadowRadius(ABTextUtil.dip2px(context, 3))
+                , shadowViewB);
+
+        // View C
+        ShadowViewHelper.bindShadowHelper(
+                new ShadowProperty()
+                        .setShadowColor(0x77000000)
+                        .setShadowRadius(ABTextUtil.dip2px(context, 6))
+                , shadowViewC);
+
+        ShadowViewHelper.bindShadowHelper(
+                new ShadowProperty()
+                        .setShadowColor(0x77000000)
+                        .setShadowRadius(ABTextUtil.dip2px(context, 4))
+                , shadowImageC);
+
+        // View D
+        int r = ABTextUtil.dip2px(context, 4);
+        ShadowViewHelper.bindShadowHelper(
+                new ShadowProperty()
+                        .setShadowColor(0x77000000)
+                        .setShadowRadius(ABTextUtil.dip2px(context, 5))
+                , shadowImageD, r, r);
+
+
+    }
+
+
+}
---
+++ library/src/main/java/com/wnagjie/shadowviewhelper/ShadowViewDrawable.java
@@ -0,0 +1,103 @@
+package com.wnagjie.shadowviewhelper;
+
+import android.graphics.*;
+import android.graphics.drawable.Drawable;
+
+/**
+ * <code>
+ * if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+ * view.setLayerType(View.LAYER_TYPE_SOFTWARE, drawable.getPaint());
+ * }
+ * </code>
+ * <p/>
+ * Author: wangjie
+ * Email: tiantian.china.2@gmail.com
+ * Date: 5/2/15.
+ */
+public class ShadowViewDrawable extends Drawable {
+    private Paint paint;
+
+    private RectF bounds = new RectF();
+    private float halfLen;
+
+    private int width;
+    private int height;
+
+    private ShadowProperty shadowProperty;
+    private int shadowOffset;
+
+    private float rx;
+    private float ry;
+
+    public ShadowViewDrawable(ShadowProperty shadowProperty, int color, float rx, float ry) {
+        this.shadowProperty = shadowProperty;
+        shadowOffset = this.shadowProperty.getShadowOffset();
+
+        this.rx = rx;
+        this.ry = ry;
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        /**
+         * 解决旋转时的锯齿问题
+         */
+        paint.setFilterBitmap(true);
+        paint.setDither(true);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(color);
+        /**
+         * 设置阴影
+         */
+        paint.setShadowLayer(shadowProperty.getShadowRadius(), shadowProperty.getShadowDx(), shadowProperty.getShadowDy(), shadowProperty.getShadowColor());
+
+    }
+
+    @Override
+    protected void onBoundsChange(Rect bounds) {
+        super.onBoundsChange(bounds);
+        if (bounds.right - bounds.left > 0 && bounds.bottom - bounds.top > 0) {
+            this.bounds.left = bounds.left;
+            this.bounds.right = bounds.right;
+            this.bounds.top = bounds.top;
+            this.bounds.bottom = bounds.bottom;
+            halfLen = Math.min(
+                    (this.bounds.right - this.bounds.left) / 2,
+                    (this.bounds.bottom - this.bounds.top) / 2
+            );
+            width = (int) (this.bounds.right - this.bounds.left);
+            height = (int) (this.bounds.bottom - this.bounds.top);
+
+            invalidateSelf();
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawRoundRect(
+                new RectF(shadowOffset, shadowOffset, width - shadowOffset, height - shadowOffset),
+                rx, ry,
+                paint
+        );
+
+    }
+
+    public ShadowViewDrawable setColor(int color) {
+        paint.setColor(color);
+        return this;
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}
